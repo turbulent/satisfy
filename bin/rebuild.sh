ddc22930d984177b716cec47d2e63ba897c5e566
Phil Rajchgot <prajchgot@turbulent.ca>
2020-02-01 21:19:39 -0500

    feat: add manual rebuild route, simply controller, fix logging

--- bin/rebuild.sh
+++ bin/rebuild.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+LOG_FILE='/tmp/satis.log'
 
-/usr/local/bin/php /var/www/satisfy/bin/console satisfy:rebuild -vvv --skip-errors /var/www/satisfy/satis.json /var/www/html > /tmp/satis.log 2>&1
-
+echo "[$(date)] Build started" >> $LOG_FILE
+/usr/local/bin/php /var/www/satisfy/bin/console satisfy:rebuild -vvv --skip-errors /var/www/satisfy/satis.json /var/www/html >> $LOG_FILE 2>&1
+echo "[$(date)] Build finished" >> $LOG_FILE
+printf "\n\n" >> $LOG_FILE
--- src/Playbloom/Satisfy/Controller/WebhookController.php
+++ src/Playbloom/Satisfy/Controller/WebhookController.php
@@ -27,37 +27,60 @@ class WebhookController implements ControllerProviderInterface
     {
         $controllers = $app['controllers_factory'];
 
-	$controllers->post('/webhook/gitlab', function (Request $request) use ($app) {
-		$data = json_decode($request->getContent(), true);
-		$gitlabSecretToken = $request->headers->get('X-Gitlab-Token');
-		$repositoryUrl = isset($data['project']['url']) ? $data['project']['url'] : null;
-		$eventType = isset($data['object_kind']) ? $data['object_kind'] : null;
+	$buildRepo = function ($url) {
+                // Apache unsets the home directory in the ENV when booting up,
+                // so we have to re-establish it or Composer will complain.
+                // See: /etc/apache2/envvars
+		putenv('HOME=/var/www');
 
-		error_log("Webhook event \"$eventType\" for repository $repositoryUrl");
+                $args = [
+                        'command' => 'build',
+                        'file' => '/var/www/satisfy/satis.json',
+                        'output-dir' => '/var/www/html',
+                        '--repository-url' => $url,
+                ];
 
-		try {
-			$repositoryId = md5($repositoryUrl);
-			$repository = $app['satis']->findOneRepository($repositoryId);
-		} catch (\Exception $e) {
-			$msg = "Repository URL $repositoryUrl is not being tracked by Satis, ignoring";
-			error_log($msg);
-			return $app->json($msg, 200);
+                $input = new ArrayInput($args);
+                $output = new ConsoleOutput();
+                $satisApp = new SatisApp();
+                return $satisApp->run($input, $output);
+	};
+
+	$findRepo = function($group, $project) use ($app) {
+		$repositories = $app['satis']->findAllRepositories();
+		$repositoryUrl = null;
+		
+		foreach ($repositories as $repo) {
+			$url = $repo->getUrl();
+			if (strpos($url, "$group/$project") !== false) {
+				$repositoryUrl = $url;
+				break;
+			}
 		}
+		
+		return $repositoryUrl;
+	};
+
+	$controllers->get('/rebuild/{group}/{project}', function (Request $request, $group, $project) use ($app, $buildRepo, $findRepo) {
+		$repositoryUrl = $findRepo($group, $project);
+		if (!$repositoryUrl) {
+			return $app->json("Repository matching $group/$project not found", 404);	
+		}
+
+		$returnCode = $buildRepo($repositoryUrl);
+		return $app->json("Return code: $returnCode", 200);
+	})
+	->bind('webhook_rebuild');
+
+	$controllers->post('/webhook/gitlab', function (Request $request) use ($app, $buildRepo, $findRepo) { 
+		$gitlabSecretToken = $request->headers->get('X-Gitlab-Token');
+		$data = json_decode($request->getContent(), true);
+		$eventType = isset($data['object_kind']) ? $data['object_kind'] : null;
+		$group = isset($data['project']['namespace']) ? $data['project']['namespace'] : null;
+		$projectName = isset($data['project']['name']) ? $data['project']['name'] : null;
 
 		if ($gitlabSecretToken !== $app['gitlab-secret-token']) {
 			$msg = 'Bad token';
-			error_log($msg);
-			return $app->json($msg, 400);
-		}
-
-		if (empty($repositoryUrl)) {
-			$msg = 'Missing repository URL';
-			error_log($msg);
-			return $app->json($msg, 400);
-		}
-
-		if (empty($eventType)) {
-			$msg = 'Missing webhook event type';
 			error_log($msg);
 			return $app->json($msg, 400);
 		}
@@ -68,23 +91,28 @@ class WebhookController implements ControllerProviderInterface
 			return $app->json($msg, 200);
 		}
 
-		// Apache unsets the home directory in the ENV when booting up,
-		// so we have to re-establish it or Composer will complain.
-		// See: /etc/apache2/envvars
-		putenv('HOME=/var/www');	
+		if (empty($eventType)) {
+			$msg = 'Missing webhook event type';
+			error_log($msg);
+			return $app->json($msg, 400);
+		}
 
-		$args = [
-			'command' => 'build',
-			'file' => '/var/www/satisfy/satis.json',
-			'output-dir' => '/var/www/html',
-			'--repository-url' => $repositoryUrl,
-		];
+		if (empty($group) || empty($projectName)) {
+			$msg = 'Missing project namespace (group) or project name';
+			error_log($msg);
+			return $app->json($msg, 400);
+		}
 
-		$input = new ArrayInput($args);
-		$output = new ConsoleOutput();
-		$satisApp = new SatisApp();
+		error_log("Webhook event \"$eventType\" for repository $group/$projectName");
 
-		$satisApp->run($input, $output);
+		$repositoryUrl = $findRepo($group, $projectName);
+		if (!$repositoryUrl) {
+			$msg = "Repository $group/$projectName is not being tracked by Satis, ignoring";
+			error_log($msg);
+			return $app->json($msg, 200);
+		}
+
+		$buildRepo($repositoryUrl);
 
 		return $app->json('OK', 200);
 	})
